121f34fc637378eeeaa642c36016b02f9c2b8b56
Ahmed Ali EL MOUTAOUADI <161154696+ahmedAliElm@users.noreply.github.com>
2024-03-21 11:35:48 +0000

    Add files via upload
    This is a simple Python project that reverses a string without using any predefined functions.

---
+++ Reversing_a_String.py
@@ -0,0 +1,16 @@
+def inversion(string):
+
+    i = 0
+    k = len(string) - 1
+    temp = ''
+
+    while i < len(string):
+        temp += string[k]
+        k -= 1
+        i += 1
+
+    return temp
+
+myString = "I love Python"
+print(inversion(myString))
+
