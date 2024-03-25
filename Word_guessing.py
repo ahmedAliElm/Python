1c95fae4dbfa44faf404a8728d1199842b82d612
Ahmed Ali EL MOUTAOUADI <161154696+ahmedAliElm@users.noreply.github.com>
2024-03-25 12:13:48 +0000

    Add files via upload

---
+++ Word_guessing.py
@@ -0,0 +1,19 @@
+def wordGuess():
+
+    word = ""  # enter a word of your choice
+    x = False  # set x to False for the while loop
+
+    while not x:  # while x == False
+
+        guess = input("Enter your guess: ")
+
+        if guess == word:
+            x = not x  # x = True
+            print("Correct guess!")
+            break
+
+        print("Wrong guess, try again")
+
+wordGuess()
+
+    
